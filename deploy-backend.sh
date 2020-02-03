4b849b3e38abda325ee50f9dcc83e6e54214babb
bendevera <ben10devera@gmail.com>
2020-02-03 14:04:00 -0700

    updated for book api

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -14,8 +14,8 @@
 
 # 1. run this script `source deploy-backend.sh`
 # 2. create virtualenv and install requirements 
-#         `virtualenv -p python3 ~/MNIST-ML-Algos-API/venv`
-#         `source ~/MNIST-ML-Algos-API/venv/bin/activate`
+#         `virtualenv -p python3 ~/book-recommender-api/venv`
+#         `source ~/book-recommender-api/venv/bin/activate`
 #         `pip3 install -r requirements.txt`
 # 3. use launch script or run gunicorn command `gunicorn app:app --daemon`
 
@@ -47,21 +47,21 @@ function setup_python_venv() {
 
     # Create virtual environment and activate it
     echo ======== Creating and activating virtual env =======
-    virtualenv -p python3 ~/MNIST-ML-Algos-API/venv
-    source ~/MNIST-ML-Algos-API/venv/bin/activate
+    virtualenv -p python3 ~/book-recommender-api/venv
+    source ~/book-recommender-api/venv/bin/activate
 }
 
 function clone_app_repository() {
     printf "***************************************************\n\t\tFetching App \n***************************************************\n"
     # Clone and access project directory
     echo ======== Cloning and accessing project directory ========
-    if [[ -d ~/MNIST-ML-Algos-API ]]; then
-        sudo rm -rf ~/MNIST-ML-Algos-API
-        git clone https://github.com/bendevera/MNIST-ML-Algos-API.git ~/MNIST-ML-Algos-API
-        cd ~/MNIST-ML-Algos-API/
+    if [[ -d ~/book-recommender-api ]]; then
+        sudo rm -rf ~/book-recommender-api
+        git clone https://github.com/bendevera/book-recommender-api.git ~/book-recommender-api
+        cd ~/book-recommender-api/
     else
-        git clone https://github.com/bendevera/MNIST-ML-Algos-API.git ~/MNIST-ML-Algos-API
-        cd ~/MNIST-ML-Algos-API/
+        git clone https://github.com/bendevera/book-recommender-api.git ~/book-recommender-api
+        cd ~/book-recommender-api/
     fi
 }
 
@@ -79,110 +79,106 @@ function setup_env() {
     sudo cat > ~/.env << EOF
     export APP_CONFIG="production"
     export SECRET_KEY="mYd3rTyL!tTl#sEcR3t"
-    export FLASK_APP=ml_algos_api.py
+    export FLASK_APP=manage.py
 EOF
     echo ======= Exporting the necessary environment variables ========
     source ~/.env
 }
 
-# Install and configure nginx
 function setup_nginx() {
     printf "***************************************************\n\t\tSetting up nginx \n***************************************************\n"
     echo ======= Installing nginx =======
-    sudo apt-get install -y nginx
+    sudo wget http://nginx.org/keys/nginx_signing.key
+    sudo apt-key add nginx_signing.key
+    echo ======= Append to sources.list =======
+    sudo bash -c 'cat <<EOF >> /etc/apt/sources.list
+    # 
+    deb http://nginx.org/packages/ubuntu xenial nginx
+    deb-src http://nginx.org/packages/ubuntu xenial nginx
+    #
+EOF'
+    sudo apt-get update
+    sudo apt-get install nginx
+    sudo service nginx start
+}
 
-    # Configure nginx routing
-    echo ======= Configuring nginx =======
-    echo ======= Removing default config =======
-    sudo rm -rf /etc/nginx/sites-available/default
-    sudo rm -rf /etc/nginx/sites-enabled/default
-    echo ======= Replace config file =======
-    sudo bash -c 'cat <<EOF > /etc/nginx/sites-available/default
+function setup_ssl () {
+    printf "***************************************************\n\t\tSetting up SSL \n***************************************************\n"
+    sudo apt-get update
+    sudo apt-get install software-properties-common
+    sudo add-apt-repository ppa:certbot/certbot
+    sudo apt-get update
+    sudo apt-get install python-certbot-nginx
+
+}
+
+# need to change nginx config file:
+# sudo nano /etc/nginx/conf.d/default.conf
+# Next to “server_name”, replace “localhost” with your domain(s)
+# sudo certbot --nginx -d <domain1 string> -d <domainN string>
+#
+
+function setup_ssl_two () {
+    printf "***************************************************\n\t\tSetting up SSL2 \n***************************************************\n"
+    cd /etc/nginx/conf.d
+    sudo mv default.conf default.conf.bak
+    sudo touch server.conf
+    sudo bash -c 'cat <<EOF > server.conf
     server {
-            listen 80 default_server;
-            listen [::]:80 default_server;
-
-            server_name _;
-
-            location / {
-                    # reverse proxy and serve the app
-                    # running on the localhost:8000
-                    proxy_pass http://127.0.0.1:8000/;
-                    proxy_set_header HOST \$host;
-                    proxy_set_header X-Forwarded-Proto \$scheme;
-                    proxy_set_header X-Real-IP \$remote_addr;
-                    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-            }
+    listen 80;
+    listen [::]:80;
+    server_name api.mnistalgotoy.com;
+    return 301 https://$server_name$request_uri;
+    }
+    server {
+    listen 443 ssl http2 default_server;
+    listen [::]:443 ssl http2 default_server;
+    server_name api.mnistalgotoy.com;
+    location / {
+    proxy_pass http://localhost:3000;
+    }
+    ssl_certificate /etc/letsencrypt/live/api.mnistalgotoy.com/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/api.mnistalgotoy.com/privkey.pem;
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers EECDH+CHACHA20:EECDH+AES128:RSA+AES128:EECDH+AES256:RSA+AES256:EECDH+3DES:RSA+3DES:!MD5;
+    ssl_session_cache shared:SSL:5m;
+    ssl_session_timeout 1h;
+    add_header Strict-Transport-Security “max-age=15768000” always;
     }
 EOF'
-
-    echo ======= Create a symbolic link of the file to sites-enabled =======
-    sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
-
-    # Ensure nginx server is running
-    echo ====== Checking nginx server status ========
-    sudo systemctl restart nginx
-    sudo nginx -t
+    sudo nginx -s reload
 }
 
 # Add a launch script
-# source ~/MNIST-ML-Algos-API/venv/bin/activate
+# source ~/book-recommender-api/venv/bin/activate
 function create_launch_script () {
     printf "***************************************************\n\t\tCreating a Launch script \n***************************************************\n"
 
     sudo cat > /home/ubuntu/launch.sh <<EOF
     #!/bin/bash
-    cd ~/MNIST-ML-Algos-API
+    cd ~/book-recommender-api
     source ~/.env
-    gunicorn app:app -D
+    gunicorn app:app --daemon
 EOF
     sudo chmod 744 /home/ubuntu/launch.sh
     echo ====== Ensuring script is executable =======
     ls -la ~/launch.sh
 }
 
-function configure_startup_service () {
-    printf "***************************************************\n\t\tConfiguring startup service \n***************************************************\n"
-
-    sudo bash -c 'cat > /etc/systemd/system/yummy-rest.service <<EOF
-    [Unit]
-    Description=yummy-rest startup service
-    After=network.target
-
-    [Service]
-    User=ubuntu
-    ExecStart=/bin/bash /home/ubuntu/launch.sh
-
-    [Install]
-    WantedBy=multi-user.target
-EOF'
-
-    sudo chmod 664 /etc/systemd/system/yummy-rest.service
-    sudo systemctl daemon-reload
-    sudo systemctl enable yummy-rest.service
-    sudo systemctl start yummy-rest.service
-    sudo service yummy-rest status
-}
-
-Serve the web app through gunicorn
-function launch_app() {
-    printf "***************************************************\n\t\tServing the App \n***************************************************\n"
-    sudo bash /home/ubuntu/launch.sh
-}
 
 ######################################################################
 ########################      RUNTIME       ##########################
 ######################################################################
 
 initialize_worker
-# setup_python_venv
+setup_python_venv
 clone_app_repository
 setup_env
-# setup_app
-setup_nginx
-create_launch_script
-configure_startup_service
-# launch_app
+setup_app
+setup_nginx # need to cross reference with blog
+# setup_ssl
+# create_launch_script # will most likely do this manually
 
 
 
